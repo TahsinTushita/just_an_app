54b7fa3d9cf396a301d1f213f9794ea73aee8096
TushitaTahsin <kazi26@student.sust.edu>
2021-07-11 18:51:15 +0600

    Connected to MongoDB

--- just_an_app_api/app.js
+++ just_an_app_api/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser= require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
-app.get('/', (req,res,next) => res.end('Welcome!!!!'));
+  MongoClient.connect('mongodb+srv://tushita:tushita123@cluster0.6zc7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+  })
+  .catch(error => console.error(error))
+
+//   const mongoose = require('mongoose');
+//   const db = 'mongodb+srv://tushita:tushita123@cluster0.6zc7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+//   mongoose
+//       .connect(db, { 
+//           useNewUrlParser: true,
+//           useCreateIndex: true
+//         })
+//       .then(() => console.log('MongoDB connected...'))
+//       .catch(err => console.log(err));
+
 
 app.listen(5000, () => console.log('Server running'));
---
+++ just_an_app_api/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://tushita:tushita123@cluster0.6zc7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
