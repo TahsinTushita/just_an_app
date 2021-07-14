ab09f4acc84369a30ceb11f2890ba2f35e83d441
TushitaTahsin <kazi26@student.sust.edu>
2021-07-14 16:48:44 +0600

    Removed csurf and fixed the CORS origin

--- just_an_app_api/server.js
+++ just_an_app_api/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const csrf = require("csurf");
 const dotenv = require("dotenv");
 
 const app = express();
-const csrfProtection = csrf();
 dotenv.config();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
@@ -20,23 +18,9 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(csrfProtection);
-// app.use(
-//   sessions({
-//     cookieName: "demo-session",
-//     secret: "this is a secret msg",
-//     duration: 30 * 60 * 1000,
-//   })
-// );
-
-// app.use(csurf({ sessionKey: "demo-session" }));
-
 const db = require("./models");
 const Role = db.role;
 const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6zc7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const db1 =
-  "mongodb+srv://tushita:tushita123@cluster0.6zc7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-//`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`
 
 db.mongoose
   .connect(connectionString, {
